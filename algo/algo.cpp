80dd7509f6ff858114a9cdbbed525f2a5463c65c
maddiii16 <salakhova.mn@gmail.com>
2024-10-02 03:16:02 +0300

    Создание структур трубы и КС. Создание функции проверки введенного числа из мен.

--- algo/algo.cpp
+++ algo/algo.cpp
@@ -1,20 +1,42 @@
-// algo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
 
-int main()
+using namespace std;
+
+struct pipe
 {
-    std::cout << "Hello World!\n";
+    string name;
+    float length;
+    int diametr;
+    bool repair;
+};
+
+struct compression_station
+{
+    string name;
+    int shopsnumber;
+    int workinshopnumber;
+    int effectiveness;
+};
+
+int checking_command_number()
+{
+    int number;
+    cin >> number;
+    while (true)
+    {
+        if ((!(cin >> number) || (cin.peek() != '\n')) && 0 < number && number > 7);
+        {
+            cout << "ќшибка! ¬ведите корректное число от 0 до 7";
+            cin.clear();
+            cin.ignore(max, '\n');
+        }
+        cin >> number;
+    }
+    return number;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int menu()
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
